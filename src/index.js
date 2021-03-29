a2c5c6d449b355cd89223902241cac1f3f1816ac
Sami <Sami0@users.noreply.github.com>
2021-03-29 16:01:56 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import TheFormView from "./TheFormView";
 document.getElementById("app").innerHTML = `
 <div class="App">
 <div><img class="ImageLaptop" src="https://storage.googleapis.com/test28764/Laptopicon.png">
-<h2> Find the perfect laptop you've been looking for</h2>
+<h2> Find a better Laptop</h2>
 <button class="submit-button" onclick="TheFormView()" > see options  </button>
 </div>
 
