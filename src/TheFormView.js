63f591b17fdb9b3ac4d1f991df700f3c52877515
Sami <Sami0@users.noreply.github.com>
2021-04-03 15:11:44 -0600

    Update TheFormView.js

--- src/TheFormView.js
+++ src/TheFormView.js
@@ -7,32 +7,35 @@ window.TheFormView = function TheFormView() {
   document.getElementById("app").innerHTML = `
   <div class="App">
 <div><img class="ImageLaptop" src="https://storage.googleapis.com/test28764/Laptopicon.png">
-<h2> Find the perfect laptop you've been looking for</h2>
+<h2> Find a better Laptop </h2>
 </div>
 <div class="App-header"> 
-<p> Q1 : which OS do you prefer ?  </p>
+<p> Which OS do you prefer ?</p>
   <form name="OSChoice">
   <!--option 3 ----->
   <!---<input type="radio" id="Mac-checked" value="MAC-OS" onclick="showLaptopBox()"> --->
   
   <input type="radio" id="MAC-OS" name="OS-choice" value="MAC" > 
   <label> Mac </label>
+  <div id="xsLabel">  the most popular </div>
   <br>
   <br>
   <!--option 2 ----->
   <input type="radio" id="UBUNTU-OS" name="OS-choice" value="Ubuntu"> 
   <label> Linux-Ubuntu  </label>
+  <div id="xsLabel"> most popular option of back-end development </div>
   <br>
   <br>
   <!--option 3 ----->
   <input type="radio" id="PURE-OS" name="OS-choice"  value="PureOS"> 
   <label> Linux- Pure OS  </label>
+  <div id="xsLabel"> <a href="https://pureos.net/ "> what is PureOS? </a> </div>
   <br>
   <br>
   <!--option 4 ----->
   <input type="radio" id="WINDOWS-OS" name="OS-choice" value="Windows"> 
   <label> Windows  </label>
-  
+  <div id="xsLabel">  directx graphics support/ better gaming  </div>
   </form>
 
   <button class="submit-button" onclick="The4QView()">see Next questions </button>
@@ -61,3 +64,4 @@ window.TheFormView = function TheFormView() {
 </div>
 `;
 };
+
